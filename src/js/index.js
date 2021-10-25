6d4ac86727b966df2730bc6d718897a3ee88415b
bojanabb <bojana.buljic@gmail.com>
2021-10-25 10:23:03 +0200

    Add functionalities to update time elapsed and update progress bar

--- src/js/index.js
+++ src/js/index.js
@@ -12,12 +12,14 @@ const playPauseBtn = document.querySelector('.js-togglePlay');
 const stopBtn = document.querySelector('.js-stop');
 const timeElapsedEl = document.querySelector('.js-time-elapsed');
 const videoDurationEl = document.querySelector('.js-duration');
-
+const progressBarEl = document.querySelector('.js-progress-bar');
+const seekEl = document.querySelector('.js-seek');
 
 
 videoEl.addEventListener('loadedmetadata', initializeVideo);
 videoEl.addEventListener('play', updatePlayBtn);
 videoEl.addEventListener('pause', updatePlayBtn);
+videoEl.addEventListener('timeupdate', updateTimeAndProgress);
 
 playPauseBtn.addEventListener('click', togglePlay);
 stopBtn.addEventListener('click', stopVideo)
@@ -58,10 +60,31 @@ function formatTime(seconds) {
     };
 }
 
+//Initializing video sets video duration and value of progress bar
 function initializeVideo() {
     const duration = Math.round(videoEl.duration);
+    seekEl.setAttribute('max', duration);
+    progressBarEl.setAttribute('max', duration);
     const time = formatTime(duration);
-
     videoDurationEl.innerText = `${time.minutes}:${time.seconds}`;
     videoDurationEl.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`);
+}
+
+//Update time while playing video
+function updateTimeElapsed() {
+    const time = formatTime(Math.round(videoEl.currentTime));
+    timeElapsedEl.innerText = `${time.minutes}:${time.seconds}`;
+    timeElapsedEl.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`);
+}
+
+//Update progress bar depending of how far video is played
+function updateProgressBar() {
+    seekEl.value = Math.floor(videoEl.currentTime);
+    progressBarEl.value = Math.floor(videoEl.currentTime);
+}
+
+
+function updateTimeAndProgress() {
+    updateTimeElapsed();
+    updateProgressBar();
 }
